732d50d5a5240fad44d2878d3e412423d87a71ca
tondemasu <tondemasu@gmail.com>
2026-05-12 13:05:13 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='pmm-pocket-web-v010';
+const CACHE='pmm-pocket-web-v011';
 const ASSETS=['./','./index.html','./styles.css','./app.js','./manifest.webmanifest','./assets/icon-192.png','./assets/icon-512.png','./assets/pmm_logo.jpg'];
 
 self.addEventListener('install', event => {
