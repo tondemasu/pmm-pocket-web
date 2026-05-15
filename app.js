b81da62ce8fffd9c879cfc1c5e9fd4f40aade325
tondemasu <tondemasu@gmail.com>
2026-05-16 00:21:07 +0900

    Add files via upload

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 'use strict';
 const $ = sel => document.querySelector(sel);
 const $$ = sel => Array.from(document.querySelectorAll(sel));
-const APP_VERSION = 'PMM Pocket Web v031';
+const APP_VERSION = 'PMM Pocket Web v032';
 const state = { data:null, fileName:'pmm_data.json', fileHandle:null, dirty:false, edit:{type:null,id:null,index:null}, previewTarget:null, photoTarget:null, crop:{img:null,scale:1,rotation:0,dx:0,dy:0,drag:false,lastX:0,lastY:0}, choice:{input:null,button:null,options:[]} };
 const typeLabels = ['P','PS','K','KS'];
 const titleOptions = ['','20p','50p','ONE','GM','PM','ECM','DCM','PDCM'];
--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='pmm-pocket-web-v031-20260513a';
-const ASSETS=['./','./index.html','./styles.css?v=031','./app.js?v=031','./manifest.webmanifest','./assets/icon-192.png','./assets/icon-512.png','./assets/pmm_logo.jpg'];
+const CACHE='pmm-pocket-web-v032-20260516a';
+const ASSETS=['./','./index.html','./styles.css?v=032','./app.js?v=032','./manifest.webmanifest','./assets/icon-192.png','./assets/icon-512.png','./assets/pmm_logo.jpg'];
 
 self.addEventListener('install', event => {
   event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)).then(() => self.skipWaiting()));
